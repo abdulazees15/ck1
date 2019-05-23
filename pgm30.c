078dad5654e725bae421a815bb0995a92c31b944
abdulazees15 <50805591+abdulazees15@users.noreply.github.com>
2019-05-23 15:47:53 +0530

    Update pgm30.c

--- pgm30.c
+++ pgm30.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
-
-int main()
+#include<stdio.h>
+#include<stdlib.h>
+void main()
 {
-   int a,b,c,i,j,k;
-   scanf("%d%d",&a,&i);
-   scanf("%d%d",&b,&j);
-   a=a-b;
-   i=i-j;
-   c=abs(a);
-   z=abs(i);
-   printf("%d %d",c,k);
-
-    return 0;
+int h1,h2,m1,m2,h,m;
+scanf("%d%d%d%d",&h1,&m1,&h2,&m2);
+h1=h1-h2;
+m1=m1-m2;
+h=abs(h1);
+m=abs(m1);
+printf("%d %d",h,m);
+    
 }
