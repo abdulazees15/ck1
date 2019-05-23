0366824d1de826021e80ce6a52d68d6d7f869276
abdulazees15 <50805591+abdulazees15@users.noreply.github.com>
2019-05-23 16:16:38 +0530

    Update pgm39.c

--- pgm39.c
+++ pgm39.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 void main()
 {
-int a[10],i,ans=0;
+int a[10],i,as=0;
 for(i=0;i<10;i++)
 {
     scanf("%d",&a[i]);
 }
-ans=a[0];
+as=a[0];
   for(i=0;i<10;i++)
   {
-      if(ans<a[i])
+      if(as<a[i])
       {
-          ans=a[i];
+          as=a[i];
       }
   }
-  printf("%d",ans);
+  printf("%d",as);
 }
