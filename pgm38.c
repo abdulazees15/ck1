f586c4925b575a70fa2f1e0a46b353c93eca8302
abdulazees15 <50805591+abdulazees15@users.noreply.github.com>
2019-05-23 16:00:18 +0530

    Create pgm38.c

---
+++ pgm38.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+   int a,b;
+   scanf("%d%d",&a,&b);
+   swap(&a,&b);
+   printf("%d %d",a,b);
+}
+void swap(int *x,int *y)
+{
+    *x=*x^*y;
+    *y=*x^*y;
+    *x=*x^*y;
+}
